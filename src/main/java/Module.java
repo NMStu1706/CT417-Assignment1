49372737d32af58841f6a0358782c4e69c2dca76
Niall Murphy <n.murphy46@nuigalway.ie>
2020-11-15 14:48:27 +0000

    All Java files ready

--- src/main/java/Module.java
+++ src/main/java/Module.java
@@ -12,13 +12,15 @@ import java.util.ArrayList;
 public class Module {
     private String moduleName;
     private String moduleId;
+    private String courses;
     private ArrayList<Student> moduleStudents=new ArrayList<Student>();
     
-    public Module(String name,String id){
+    public Module(String name,String id,String courses){
         moduleName=name;
         moduleId=id;
+        this.courses=courses;
     }
-    
+    //getters
     public String getModuleName(){
         return moduleName;
     }
@@ -26,13 +28,18 @@ public class Module {
     public String getModuleID(){
         return moduleId;
     }
-    
+    //add student to module
     public void addStudent(Student stu){
         moduleStudents.add(stu);
     }
-    
+    //get array
     public ArrayList getStudents(){
         return moduleStudents;
+    }
+    
+    @Override
+    public String toString(){
+        return "*****\n Module Name: "+moduleName+"\nModule ID: "+moduleId+"\n*****";
     }
     
 }
--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -9,6 +9,7 @@
  * @author Niall Murphy
  */
 public class Student {
+    //initialise relevant variables 
     private String name;
     private String courses;
     private String modules;
@@ -17,6 +18,7 @@ public class Student {
     private double id;
     
     public Student(String name, int age,String courses,String modules){
+        //assign given variables
         this.name=name;
         this.age=age;
         this.courses=courses;
@@ -24,7 +26,7 @@ public class Student {
         id = (999999)*Math.random();
         getUsername();
     }
-    
+    //getter methods 
     public String getName(){
         return name;
     }
@@ -40,9 +42,14 @@ public class Student {
     public String getModules(){
         return modules;
     }
-    
+    //concat username from name and age
     public String getUsername(){
         username=name+age;
         return username;
+    }
+    
+    @Override
+    public String toString(){
+        return "*****\n Student Name: "+name+"\nUsername: "+username+"\n*****";
     }
 }
