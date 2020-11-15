618fcedbdd1577bf11b557534a825092eb432e68
Niall Murphy <n.murphy46@nuigalway.ie>
2020-11-15 14:48:13 +0000

    All Java files ready

--- src/main/java/CourseProgramme.java
+++ src/main/java/CourseProgramme.java
@@ -8,6 +8,48 @@
  *
  * @author User1
  */
+import org.joda.time.*;
+import java.util.ArrayList;
 public class CourseProgramme {
+    private DateTime start;
+    private DateTime end;
+    private String courseName;
+    private ArrayList<Student> courseStudents=new ArrayList<Student>();
+    private ArrayList<Module> courseModules=new ArrayList<Module>();
+    
+    
+    public CourseProgramme(DateTime start,DateTime end, String courseName){
+        this.start=start;
+        this.end=end;
+        this.courseName=courseName;
+    }
+    //getters
+    private String getCourseName(){
+        return courseName;
+    }
+    
+    private DateTime getStart(){
+        return start;
+    }
+    
+    private DateTime getEnd(){
+        return end;
+    }
+    //append to arrays
+    private void addModule(Module m){
+        courseModules.add(m);
+    }
+    
+    private void addStudent(Student s){
+        courseStudents.add(s);
+    }
+    //get arrays
+    private ArrayList getModules(){
+        return courseModules;
+    }
+    
+    private ArrayList getStudents(){
+        return courseStudents;
+    }
     
 }
